89ed55a3fde4628c9f14a79c8936c79f9b36b47b
a.nikiforov <933Y%eq$TFd]#msUGFG1>
2025-02-14 15:12:56 +0500

    locust подправил

--- locustfile.py
+++ locustfile.py
@@ -1,10 +1,17 @@
 import random
 from locust import HttpUser, task, between
 
-class ComputeUser(HttpUser):
-    wait_time = between(1, 2)
+class LoadTestUser(HttpUser):
+    wait_time = between(1, 2)  # Задержка между запросами
 
     @task
-    def compute(self):
-        n = random.randint(1, 50)  # Генерируем случайное число от 1 до 100
-        self.client.get(f"/api/compute?n={n}")
+    def compute_request(self):
+        n = random.randint(1, 50)
+        with self.client.get(f"/api/compute?n={n}", catch_response=True) as response:
+            if response.status_code == 200:
+                print(f"Response: {response.text}")  # Вывод в консоль
+                response.success()
+            else:
+                error_message = f"Error {response.status_code}: {response.text}"
+                print(error_message)  # Вывод в консоль
+                response.failure(error_message)
